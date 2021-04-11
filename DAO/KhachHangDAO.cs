cbae2962d66ae43ab7cdb130febc79442cf41a75
Zyr0nX <55247256+Zyr0nX@users.noreply.github.com>
2021-04-11 19:14:18 +0700

    Almost done tab KhachHang
    remaining button: clear, delete

---
+++ DAO/KhachHangDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachHang.DAO
+{
+    class KhachHangDAO
+    {
+        private static KhachHangDAO instance;
+
+        internal static KhachHangDAO Instance
+        {
+            get { if (instance == null) instance = new KhachHangDAO(); return instance; }
+            private set { instance = value; }
+        }
+
+        private KhachHangDAO() { }
+
+        public string getTenKHbyMaKH(string makh)
+        {
+            string data = DataProvider.Instance.executeScalar("select TENKH from KHACHHANG where MAKH = '" + makh + "'").ToString();
+            return data;
+        }
+        public DataTable getKHList()
+        {
+            DataTable table = DataProvider.Instance.executeQuery("select * from KHACHHANG");
+            return table;
+        }
+        public DataTable timKiemKH(string makh, string tenkh)
+        {
+            string query = string.Format("SELECT * FROM KHACHHANG WHERE(MAKH LIKE '%' + '{0}' + '%' OR '{0}' = '') AND(TENKH LIKE N'%' + '{1}' + N'%' OR '{1}' = '')", makh, tenkh);
+            DataTable table = DataProvider.Instance.executeQuery(query);
+            return table;
+        }
+    }
+}
